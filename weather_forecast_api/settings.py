f25792e412840d6863dd00d58eaa9c98034e0c1a
ernesto <ernestozarza91@yahoo.com>
2019-05-23 17:22:26 +0200

    Setting the configuration to protect the API key

--- weather_forecast_api/settings.py
+++ weather_forecast_api/settings.py
@@ -11,13 +11,15 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+SITE_DOMAIN = 'http://weatherinfo.ez'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ybsv1-x^f=^(t@%+jav-pdrr724gmk=eof_b+e!=w!e9t!t3i3'
@@ -26,7 +28,6 @@ SECRET_KEY = 'ybsv1-x^f=^(t@%+jav-pdrr724gmk=eof_b+e!=w!e9t!t3i3'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'weather_forecast_api.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -84,7 +84,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -104,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -118,8 +116,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+WEATHER_API_KEY = config('WEATHER_APP_ID', default='')
